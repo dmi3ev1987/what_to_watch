8d0623b8585691d54655f01e8c8cd51bffc86cfb
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2024-08-17 13:31:06 +0300

    SQL.

--- opinions_app.py
+++ opinions_app.py
@@ -1,11 +1,21 @@
 from flask import Flask
 
+# Импортируется нужный класс:
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+# Подключаем БД SQLite:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# Создаётся экземпляр класса SQLAlchemy и передаётся
+# в качестве параметра экземпляр приложения Flask:
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index_view():
     return 'Совсем скоро тут будет случайное мнение о фильме!'
+
 
 if __name__ == '__main__':
     app.run()
