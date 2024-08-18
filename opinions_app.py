1547c6f6a24380f87461d9875f8816dcbf39850a
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2024-08-18 13:13:25 +0300

    Add db.

--- opinions_app.py
+++ opinions_app.py
@@ -1,21 +1,49 @@
-from flask import Flask
+from datetime import datetime
 
-# Импортируется нужный класс:
+# Импортируется функция для выбора случайного значения:
+from random import randrange
+
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # Подключаем БД SQLite:
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 # Создаётся экземпляр класса SQLAlchemy и передаётся
 # в качестве параметра экземпляр приложения Flask:
 db = SQLAlchemy(app)
 
 
-@app.route('/')
+class Opinion(db.Model):
+    # ID — целое число, первичный ключ:
+    id = db.Column(db.Integer, primary_key=True)
+    # Название фильма — строка длиной 128 символов, не может быть пустым:
+    title = db.Column(db.String(128), nullable=False)
+    # Мнение о фильме — большая строка, не может быть пустым,
+    # должно быть уникальным:
+    text = db.Column(db.Text, unique=True, nullable=False)
+    # Ссылка на сторонний источник — строка длиной 256 символов:
+    source = db.Column(db.String(256))
+    # Дата и время — текущее время,
+    # по этому столбцу база данных будет проиндексирована:
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+
+@app.route("/")
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    # Определяется количество мнений в базе данных:
+    quantity = Opinion.query.count()
+    # Если мнений нет...
+    if not quantity:
+        # ...то возвращается сообщение:
+        return "В базе данных мнений о фильмах нет."
+    # Иначе выбирается случайное число в диапазоне от 0 до quantity...
+    offset_value = randrange(quantity)
+    # ...и определяется случайный объект:
+    opinion = Opinion.query.offset(offset_value).first()
+    return opinion.text
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
