ce5b80d66e6e59b6c2e96d1bbdff0ad247f21e1e
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2024-08-21 15:08:47 +0300

    Exception.

--- opinions_app/api_views copy.py
+++ opinions_app/api_views copy.py
@@ -1,7 +1,6 @@
 from flask import jsonify, request
 
 from . import app, db
-from .error_handlers import InvalidAPIUsage
 from .models import Opinion
 from .views import random_opinion
 
@@ -9,10 +8,8 @@ from .views import random_opinion
 # Явно разрешаем метод GET:
 @app.route('/api/opinions/<int:id>/', methods=['GET'])
 def get_opinion(id):
-    opinion = Opinion.query.get(id)
-    if opinion is None:
-        # Тут код ответа нужно указать явным образом.
-        raise InvalidAPIUsage('Мнение с указанным id не найдено', 404)
+    # Получаем объект по id или выбрасываем ошибку 404:
+    opinion = Opinion.query.get_or_404(id)
     return jsonify({'opinion': opinion.to_dict()}), 200
 
 
@@ -23,12 +20,13 @@ def update_opinion(id):
         'text' in data
         and Opinion.query.filter_by(text=data['text']).first() is not None
     ):
-        # Выбрасываем собственное исключение.
-        raise InvalidAPIUsage('Такое мнение уже есть в базе данных')
-    opinion = Opinion.query.get(id)
-    # Тут код ответа нужно указать явным образом.
-    if opinion is None:
-        raise InvalidAPIUsage('Мнение с указанным id не найдено', 404)
+        # При неуникальном значении поля text
+        # возвращаем сообщение об ошибке в формате JSON
+        # и статус-код 400:
+        return jsonify({'error': 'Такое мнение уже есть в базе данных'}), 400
+    # Если метод get_or_404 не найдёт указанный ID,
+    # то он выбросит исключение 404:
+    opinion = Opinion.query.get_or_404(id)
     opinion.title = data.get('title', opinion.title)
     opinion.text = data.get('text', opinion.text)
     opinion.source = data.get('source', opinion.source)
@@ -43,10 +41,7 @@ def update_opinion(id):
 
 @app.route('/api/opinions/<int:id>/', methods=['DELETE'])
 def delete_opinion(id):
-    opinion = Opinion.query.get(id)
-    if opinion is None:
-        # Тут код ответа нужно указать явным образом.
-        raise InvalidAPIUsage('Мнение с указанным id не найдено', 404)
+    opinion = Opinion.query.get_or_404(id)
     db.session.delete(opinion)
     db.session.commit()
     # При удалении принято возвращать только код ответа 204:
@@ -66,15 +61,21 @@ def get_opinions():
 @app.route('/api/opinions/', methods=['POST'])
 def add_opinion():
     # Получение данных из запроса в виде словаря:
-    data = request.get_json()
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({'error': 'Заполните поля'}), 400
+    # Если нужных ключей нет в словаре...
     if 'title' not in data or 'text' not in data:
-        # Выбрасываем собственное исключение.
-        # Второй параметр (статус-код) в этом обработчике можно не передавать:
-        # нужно вернуть код 400, а он и так возвращается по умолчанию.
-        raise InvalidAPIUsage('В запросе отсутствуют обязательные поля')
+        # ...то возвращаем сообщение об ошибке в формате JSON и код 400:
+        return jsonify(
+            {'error': 'В запросе отсутствуют обязательные поля'}
+        ), 400
+    # Если в базе данных уже есть объект
+    # с таким же значением поля text...
     if Opinion.query.filter_by(text=data['text']).first() is not None:
-        # Выбрасываем собственное исключение.
-        raise InvalidAPIUsage('Такое мнение уже есть в базе данных')
+        # ...возвращаем сообщение об ошибке в формате JSON
+        # и статус-код 400:
+        return jsonify({'error': 'Такое мнение уже есть в базе данных'}), 400
     # Создание нового пустого экземпляра модели:
     opinion = Opinion()
     # Наполнение экземпляра данными из запроса:
@@ -89,6 +90,4 @@ def add_opinion():
 @app.route('/api/get-random-opinion/', methods=['GET'])
 def get_random_opinion():
     opinion = random_opinion()
-    if opinion is not None:
-        return jsonify({'opinion': opinion.to_dict()}), 200
-    raise InvalidAPIUsage('В базе данных нет мнений', 404)
+    return jsonify({'opinion': opinion.to_dict()}), 200
